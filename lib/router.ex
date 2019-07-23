2874c96560192b2033317e4e378cd14855b560f3
Jake Pitman <jakeseanp@gmail.com>
2019-07-23 11:44:36 +1000

    add remaining endpoints

--- lib/router.ex
+++ lib/router.ex
@@ -8,4 +8,25 @@ defmodule ToyRobot.Router do
     robot = GenServer.call(ToyRobot.Game.Robot, :move)
     send_resp(conn, 200, inspect(robot))
   end
+
+  post "/rotate_left" do
+    robot = GenServer.call(ToyRobot.Game.Robot, :rotate_left)
+    send_resp(conn, 200, inspect(robot))
+  end
+
+  post "/rotate_right" do
+    robot = GenServer.call(ToyRobot.Game.Robot, :rotate_right)
+    send_resp(conn, 200, inspect(robot))
+  end
+
+  post "/place/:coords" do
+    robot = GenServer.call(ToyRobot.Game.Robot, {:place, coords})
+    send_resp(conn, 200, inspect(robot))
+  end
+
+  post "/report" do
+    robot = GenServer.call(ToyRobot.Game.Robot, :report)
+    send_resp(conn, 200, inspect(robot))
+  end
+
 end

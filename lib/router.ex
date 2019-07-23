3e3f0a3c66ac05f2ef14ef15e1bb41205c7f4d96
Jake Pitman <jakeseanp@gmail.com>
2019-07-23 11:14:08 +1000

    add missing plugs to router

--- lib/router.ex
+++ lib/router.ex
@@ -1,6 +1,9 @@
 defmodule ToyRobot.Router do
   use Plug.Router
 
+  plug :match
+  plug :dispatch
+
   post "/move" do
     robot = GenServer.call(ToyRobot.Game.Robot, :move)
     send_resp(conn, 200, inspect(robot))
